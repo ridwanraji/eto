1ca96fb4be7c0b512f06294bd3759e909a885b99
RIDWAN RAJI <ror716@mail.usask.ca>
2019-04-11 10:10:03 -0600

    Initial Page setup

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
+<v-app dark>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img class="edo-logo" src="./assets/Eto-bottom-tp.png">
+    <Home msg="Welcome to Eto."/>
   </div>
+</v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Home from './components/Home.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Home
   }
 }
 </script>
@@ -22,7 +24,25 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: white;
+  background-color: #0d0e14;
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  position: relative;
+}
+
+.edo-logo{
+  margin-top: 300px;
+  width: 200px; 
+  height: 200px;
+}
+
+@media (max-width: 768px){
+  .edo-logo{
+  margin-top: 150px;
+  width: 170px; 
+  height: 190px;
+  }
 }
 </style>
---
+++ src/components/Home.vue
@@ -0,0 +1,27 @@
+<template>
+<v-app dark>
+  <div id="home">
+    <h1>{{ msg }}</h1>
+    <p>
+      We will help you organize and manage bookings for services you render.
+    </p>
+  </div>
+</v-app>
+  
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1 {
+  color: white;
+}
+</style>
