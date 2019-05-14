cc4c379fab13284c3ccbf639647370a146c9c853
RIDWAN RAJI <ror716@mail.usask.ca>
2019-05-14 11:13:26 -0600

    Update home-page with new look

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,10 +1,20 @@
 <template>
 <v-app dark>
   <div id="home">
-    <h1>{{ msg }}</h1>
+    <img class="edo-logo" src="../assets/Eto-bottom-tp.png">
+    <h1>Welcome to Eto.</h1>
     <p>
       We will help you organize and manage bookings for services you render.
     </p>
+  </div>
+
+  <div class="buttons text-xs-center">
+    <router-link to="/signin">
+      <v-btn outline round>Sign in</v-btn>
+    </router-link>
+    <router-link to="/signup">
+      <v-btn outline round>Sign Up</v-btn>
+    </router-link>
   </div>
 </v-app>
   
@@ -12,15 +22,35 @@
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
+  name: 'Home',
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+
+.edo-logo{
+  margin-top: 300px;
+  width: 200px; 
+  height: 200px;
+}
+
+@media (max-width: 1920px){
+  .edo-logo{
+  margin-top: 200px;
+  width: 170px; 
+  height: 190px;
+  }
+}
+
+@media (max-width: 768px){
+  .edo-logo{
+  margin-top: 150px;
+  width: 170px; 
+  height: 190px;
+  }
+}
+
 h1 {
   color: white;
 }
