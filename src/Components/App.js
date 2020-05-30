c2091620f42c52e926bf68ba9267f458956fbb81
RIDWAN RAJI <ror716@mail.usask.ca>
2020-05-30 08:55:24 -0600

    add logo to homepage

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,11 +1,12 @@
 import React from "react";
+import logo from "./../assets/Eto-bottom-tp.png"
 import "./../styles/App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h3>ETO APP</h3>
+      <img src={logo} className="App-logo" alt="logo" />
       </header>
     </div>
   );
