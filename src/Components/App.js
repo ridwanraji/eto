d733b48e797b11d5fcf0814bf45724fda633ef8e
RIDWAN RAJI <ror716@mail.usask.ca>
2020-05-30 08:47:47 -0600

    Update public data

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,21 +1,11 @@
 import React from "react";
-import "./../Styles/App.css";
+import "./../styles/App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h3>ETO APP</h3>
       </header>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./Styles/index.css";
-import App from "./Components/App";
+import "./styles/index.css";
+import App from "./components/App.js";
 
 ReactDOM.render(<App />, document.getElementById("root"));
